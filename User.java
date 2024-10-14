60ad996865d3412da12f011fc9eebfb990d14f3e
RichardMilos233 <160740235+RichardMilos233@users.noreply.github.com>
2024-10-14 19:40:41 +0800

    updated User.java

--- User.java
+++ User.java
@@ -1,18 +1,33 @@
+import java.util.*;
+
 public class User {
 
-	private string hospitalID;
-	private string password;
-	private string name;
-	private boolean gender;
-	private int age;
+	private String hospitalID = '';
+	private String password = '114514';
+	private String name = '';
+	private boolean gender = false; // false for male, true for female
+	private int age = -1;
 
+	Scanner scanner = new Scanner(System.in);
 	public User() {
 		// TODO - implement User.User
 		throw new UnsupportedOperationException();
 	}
 
+	public User(String hospitalID, String password, String name, boolean gender, int age) {
+		// TODO - implement User.User
+		this.hospitalID = hospitalID;
+		this.password = password;
+		this.name = name;
+		this.gender = gender;
+		this.age = age;
+		throw new UnsupportedOperationException();
+	}
+
 	public void login() {
 		// TODO - implement User.login
+		String enteredPassword = '';
+		
 		throw new UnsupportedOperationException();
 	}
 
@@ -23,6 +38,10 @@ public class User {
 
 	public void changePassword() {
 		// TODO - implement User.changePassword
+		String newPassword = '';
+		System.out.print("Enter new password: ");
+		newPassword = scanner.nextLine();
+		this.password = newPassword;
 		throw new UnsupportedOperationException();
 	}
 
