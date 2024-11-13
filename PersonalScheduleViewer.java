6eb7e4c14c36c002cafd86a42f8ffcfb539b6299
RichardMilos233 <richardmilos233@gmail.com>
2024-11-14 00:22:37 +0800

    modified:   PersonalScheduleViewer.java

--- PersonalScheduleViewer.java
+++ PersonalScheduleViewer.java
@@ -5,11 +5,16 @@ public class PersonalScheduleViewer {
         List<Appointment> timeTable = doctor.getTimeTable();
         Appointment appointment;
         int i;
+        int count = 0;
         for (i = 0; i < timeTable.size(); i++){
             appointment = timeTable.get(i);
             if (appointment.getStatus().equals("confirmed")){
                 appointment.displayAppointment();
+                count ++;
             }
+        }
+        if (count == 0){
+            System.out.println("There is no upcoming cofirmed appointment");
         }
     }
 }
