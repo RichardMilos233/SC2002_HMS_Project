a2736b47f2e269f25707b2a209dd4aecb4464f36
RichardMilos233 <richardmilos233@gmail.com>
2024-11-20 11:10:19 +0800

    renamed:    hms/account/AccountInfo.java -> hms/account/account-info.java 	renamed:    hms/appointment/AppointmentInfo.java -> hms/appointment/appointment-info.java 	renamed:    hms/inventory/InventoryInfo.java -> hms/inventory/inventory-info.java 	renamed:    hms/medicalrecords/MedicalrecordsInfo.java -> hms/medicalrecords/medicalrecords-info.java 	renamed:    hms/storage/StorageInfo.java -> hms/storage/storage-info.java 	new file:   hms/utils/billing/billing-info.java 	new file:   hms/utils/cryptography/cryptography-info.java 	renamed:    hms/utils/UtilsInfo.java -> hms/utils/utils-info.java 	renamed:    hms/views/ViewsInfo.java -> hms/views/views-info.java

---
+++ hms/utils/billing/billing-info.java
@@ -0,0 +1,6 @@
+
+/**
+ * Provides the classes and interfaces for handling the billing of medications in a healthcare management system.
+ * This package includes functionality for calculating medication fees based on unit prices and quantities.
+ */
+package hms.utils.billing;
---
+++ hms/utils/cryptography/cryptography-info.java
@@ -0,0 +1,5 @@
+/**
+ * Provides classes and interfaces for cryptographic operations such as hashing passwords combined with salts,
+ * designed to enhance the security of storing and verifying user credentials.
+ */
+package hms.utils.cryptography;
